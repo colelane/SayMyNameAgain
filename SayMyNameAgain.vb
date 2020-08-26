1cd032b4d803459d1a8572aef1f91c38c139ffd6
Lane Coleman <lane.coleman@live.com>
2020-08-25 23:44:51 -0600

    Add project files.

---
+++ SayMyNameAgain.vb
@@ -0,0 +1,24 @@
+﻿Option Strict On
+Option Explicit On
+
+'Lane Coleman
+'RCET0265
+'Spring 2020
+'Say My Name Again
+
+Module SayMyNameAgain
+
+    Sub Main()
+        Dim userInput As String
+        Console.WriteLine("What is your name?")
+        userInput = Console.ReadLine()
+        If userInput = "Emily" Then Console.WriteLine("Hi Emily.")
+        If userInput = "Joe" Then Console.WriteLine("Hi Joe.")
+        If userInput = "Lane" Then Console.WriteLine("Bruh")
+        If userInput = "Tim" Then Console.WriteLine("Rossiter?  The man, the myth, the legend?")
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
