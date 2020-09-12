535d19ca9acfcd94c39cf27cb55ff34bba495db0
Tim Rossiter <48777447+rosstimo@users.noreply.github.com>
2020-09-12 06:44:39 -0600

    Update SayMyNameAgain.vb
    add Else to cover anything that didn't evaluate to True in the If/ElseIf sections - TJR

--- SayMyNameAgain.vb
+++ SayMyNameAgain.vb
@@ -18,22 +18,15 @@ Module SayMyNameAgain
         'individual reactions for specific names
         If userInput = "Emily" Then
             Console.WriteLine("Hi Emily.")
-
         ElseIf userInput = "Joe" Then
             Console.WriteLine("Joe mama.")
-
         ElseIf userInput = "Lane" Then
             Console.WriteLine("Bruh")
-
         ElseIf userInput = "Tim" Then
             Console.WriteLine("Rossiter?  Uh oh... uhm, I gotta go")
-        ElseIf _
-            userInput <> "Emily" And
-            userInput <> "Joe" And
-            userInput <> "Lane" And
-            userInput <> "Tim" Then
+        Else
+            'Else will cover anything that didn't evaluate to True above - TJR
             Console.WriteLine("Go away")
-
         End If
 
         Console.ReadLine()
